dc287c40a683abdf95856ef5cd891f7ea8900fce
xmchang <xmchang@Ctrip.com>
2014-10-30 21:08:48 +0800

    comit

--- GitTest/src/HelloGit.java
+++ GitTest/src/HelloGit.java
@@ -8,7 +8,8 @@ public class HelloGit {
 		System.out.println("Hello Git, 2014-01-02");
 		System.out.println("Hello Git, 2014-01-03");
 		System.out.println("Hello Git, 2014-01-04");
-		System.out.println("Hello Git, test code reivew 2014-01-05");
+		//System.out.println("Hello Git, test code reivew 2014-01-05");
+		System.out.println("Hello Git, test code reivew new 2014-01-05");
 	}
 
 }
