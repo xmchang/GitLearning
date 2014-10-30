7842960629772bb4b89f283b9b23e859d44f7226
xmchang <xmchang@Ctrip.com>
2014-10-30 20:11:59 +0800

    add 2014-01-02

--- GitTest/src/HelloGit.java
+++ GitTest/src/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git, 2014-01-01");
 		System.out.println("Get from remote dev branch");
-
+		System.out.println("Hello Git, 2014-01-02");
 	}
 
 }
