ae4a545d57466d6a6432c6875d44ed0a65d087c8
xmchang <xmchang@Ctrip.com>
2014-10-30 20:05:52 +0800

    add get from dev branch

--- GitTest/src/HelloGit.java
+++ GitTest/src/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git, 2014-01-01");
+		System.out.println("Get from remote dev branch");
 
 	}
 
